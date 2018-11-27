5812b34522f4ff2714666d6792370fe22ff83908
Javier Luraschi <javierluraschi@hotmail.com>
2018-11-26 16:49:45 -0800

    extract arrow sources for build

--- r/tools/winlibs.R
+++ r/tools/winlibs.R
@@ -1,8 +1,6 @@
 VERSION <- commandArgs(TRUE)
 
-if (dir.exists("../arrow")) {
-  dir.create("../arrow")
-}
+if (!dir.exists("../arrow")) dir.create("../arrow")
 
 download_name <- sprintf("../arrow/apache-arrow-%s.tar.gz", VERSION)
 download_url <- sprintf("http://archive.apache.org/dist/arrow/arrow-%s/apache-arrow-%s.tar.gz", VERSION, VERSION)
@@ -10,4 +8,9 @@ download_url <- sprintf("http://archive.apache.org/dist/arrow/arrow-%s/apache-ar
 if(!file.exists(download_name)) {
   if(getRversion() < "3.3.0") setInternet2()
   download.file(download_url, download_name, quiet = TRUE)
+}
+
+extract_path <- sprintf("../arrow/apache-arrow-%s", VERSION)
+if(!dir.exists(extract_path)) {
+  untar(download_name, exdir = "../arrow")
 }
